dac0dfa15e827d257ec0529bc7498bf1cf110561
megha <megha@MEGHAS_VICTUS>
2024-12-03 11:03:59 +0530

    Add project files.

---
+++ learning/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+namespace Iteration
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            var name = "John Smith";
+            int j = name.Length;
+            for (var i = 0; i < j; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+            Program two = new Program();
+            two.Two();
+            Program three = new Program();
+            three.Three();
+            Smile smile = new Smile();
+            smile.Main();
+          
+
+
+            
+        }
+        public void Two()
+        {
+            var name = "John smith";
+            foreach(var character in name)
+            {
+                Console.WriteLine(character);
+            }
+        }
+        public void Three()
+        {
+            var names = new int[] { 1, 2, 3, 4 };
+            foreach (var name in names)
+            { Console.WriteLine(name); }
+        }
+    }
+}
---
+++ learning/Smile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iteration
+{
+     class Smile
+    {
+        public void Main()
+        {
+            
+            while(true)
+            {
+                Console.WriteLine("enteryour name;");
+                var input = Convert.ToInt32(Console.ReadLine());
+              
+                if (input != 0)
+                {
+                    Console.WriteLine("@echo = " + input);
+                    continue;
+                }
+                else
+                { Console.WriteLine("Please enter a name"); }
+                break;
+            }
+           
+        }
+        public void Main2() 
+        {
+            var Random = new Random();
+            
+            Console.WriteLine(Random.Next(1, 10));
+        }
+
+    }
+}
